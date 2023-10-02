3d5a7e7937f4262abd0e3d701f7aa53f452ab280
sukinnamz <innama.maesaputri@gmail.com>
2023-10-02 14:00:14 +0700

    setelah diubah

--- daspro-jobsheet6/Pemilihan2Percobaan315.java
+++ daspro-jobsheet6/Pemilihan2Percobaan315.java
@@ -12,7 +12,7 @@ public class Pemilihan2Percobaan315 {
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input15.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000) {
                 pajak = 0.1;
             } else if (penghasilan <= 3000000) {
@@ -22,7 +22,7 @@ public class Pemilihan2Percobaan315 {
             }
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasilan bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000) {
                 pajak = 0.15;
             } else if (penghasilan <= 3500000) {
