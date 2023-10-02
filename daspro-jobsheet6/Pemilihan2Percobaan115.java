9cc6f7957e7c9c7702e486bc23576042484bb7ca
sukinnamz <innama.maesaputri@gmail.com>
2023-10-02 11:40:24 +0700

    Percobaan 1

---
+++ daspro-jobsheet6/Pemilihan2Percobaan115.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan115 {
+    public static void main(String[] args) {
+        Scanner input15 = new Scanner(System.in);
+        System.out.print("Masukkan Tahun : ");
+        int tahun = input15.nextInt();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
