0cab4b1e1d3743b69cf5049af1faff67839d2fce
sukinnamz <innama.maesaputri@gmail.com>
2023-10-02 12:16:43 +0700

    diubah setelah pertanyaan 4

--- daspro-jobsheet6/Pemilihan2Percobaan115.java
+++ daspro-jobsheet6/Pemilihan2Percobaan115.java
@@ -6,8 +6,12 @@ public class Pemilihan2Percobaan115 {
         System.out.print("Masukkan Tahun : ");
         int tahun = input15.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahunbn Kabisat");
+                }
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
