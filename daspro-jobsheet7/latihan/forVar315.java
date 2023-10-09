d5c6b029ffb4ccefb1d170af39c9af61598c9ec5
sukinnamz <innama.maesaputri@gmail.com>
2023-10-09 14:11:10 +0700

    var 3

---
+++ daspro-jobsheet7/latihan/forVar315.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class forVar315 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bil, n;
+        boolean berhenti = false;
+        for (n = 0; !berhenti; n++) {
+            System.out.print("Masukkan bilangan : ");
+            bil = sc.nextInt();
+            System.out.println("Bilangan yang anda masukkan : " + bil);
+            if (bil < n) {
+                berhenti = true;
+            }
+        }
+        System.out.println("Program berakhir");
+    }
+}
