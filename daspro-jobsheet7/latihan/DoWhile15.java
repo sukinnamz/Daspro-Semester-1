33fe73f5cb4f447ab8973717dfcc39c12f5b0594
sukinnamz <innama.maesaputri@gmail.com>
2023-10-09 14:30:24 +0700

    do while

---
+++ daspro-jobsheet7/latihan/DoWhile15.java
@@ -0,0 +1,16 @@
+public class DoWhile15 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        int X = 10;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+    }
+
+}
