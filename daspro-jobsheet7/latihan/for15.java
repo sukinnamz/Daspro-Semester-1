9a561e551df97a6fd939be3d575a394908538c97
sukinnamz <innama.maesaputri@gmail.com>
2023-10-09 12:29:29 +0700

    latihan for 1

---
+++ daspro-jobsheet7/latihan/for15.java
@@ -0,0 +1,7 @@
+public class for15 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
+        }
+    }    
+}
