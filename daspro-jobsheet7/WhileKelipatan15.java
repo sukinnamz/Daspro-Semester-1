7909fecbe0ea68d07f7c467657aa6baf3c83e0c1
sukinnamz <innama.maesaputri@gmail.com>
2023-10-09 11:20:49 +0700

    pertanyaan 3 dan 4

--- daspro-jobsheet7/WhileKelipatan15.java
+++ daspro-jobsheet7/WhileKelipatan15.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ForKelipatan15 {
+public class WhileKelipatan15 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, rataRata = 0, total = 0, counter = 0;
@@ -8,13 +8,16 @@ public class ForKelipatan15 {
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
 
-        for (int i = 1; i <= 50; i++) {
+        int i = 1;
+        while (i <= 50) {
             if (i % kelipatan == 0) {
                 total += i;
                 counter++;
                 rataRata = total / counter;
             }
+            i++;
         }
+
         System.out.printf("Banyaknya %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
         System.out.printf("Rata rata adalah adalah " + rataRata);
