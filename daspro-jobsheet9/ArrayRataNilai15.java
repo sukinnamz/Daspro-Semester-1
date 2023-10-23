f865eccbae5a7c00aa90e441bb9b26d4f9a69022
sukinnamz <innama.maesaputri@gmail.com>
2023-10-23 14:25:34 +0700

    Percobaan 3 Pertanyaan 2

--- daspro-jobsheet9/ArrayRataNilai15.java
+++ daspro-jobsheet9/ArrayRataNilai15.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class ArrayRataNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
+        int banyak;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        banyak = sc.nextInt();
+        int[] nilaiMhs = new int[banyak];
         double total = 0;
         double rata2;
         int lulus = 0;
@@ -18,7 +21,9 @@ public class ArrayRataNilai15 {
             }
         }
         rata2 = total / nilaiMhs.length;
-        System.out.println("rata-rata nilai = " + rata2);
+        System.out.println("rata-rata nilai lulus = " + rata2);
         System.out.println("Banyaknya mahasiswa yang lulus adalah " + lulus + " orang");
+
+        
     }
 }
