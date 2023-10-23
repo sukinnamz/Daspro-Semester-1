5e7413c289abcf48d22fe94d514aa08ed9cb768b
sukinnamz <innama.maesaputri@gmail.com>
2023-10-23 12:22:05 +0700

    Percobaan 3

--- daspro-jobsheet9/ArrayNilai15.java
+++ daspro-jobsheet9/ArrayNilai15.java
@@ -9,7 +9,9 @@ public class ArrayNilai15 {
             nilaiAkhir[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }
         }
     }
 }
