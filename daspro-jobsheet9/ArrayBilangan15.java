f114eb5415443135b9faea384e3dcc9ee9e054cf
sukinnamz <innama.maesaputri@gmail.com>
2023-10-23 11:52:21 +0700

    Pertanyaan 5

--- daspro-jobsheet9/ArrayBilangan15.java
+++ daspro-jobsheet9/ArrayBilangan15.java
@@ -1,7 +1,7 @@
 public class ArrayBilangan15 {
     public static void main(String[] args) {
         int[] bil = { 5, 13, -7, 17 };
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil[i]);
         }
     }
