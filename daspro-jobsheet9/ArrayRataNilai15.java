06c7b547acd10cfa9d2c37cc7d540fdfc1b73c0b
sukinnamz <innama.maesaputri@gmail.com>
2023-10-23 14:12:54 +0700

    Percobaan 3 pertanyaan 1

--- daspro-jobsheet9/ArrayRataNilai15.java
+++ daspro-jobsheet9/ArrayRataNilai15.java
@@ -6,14 +6,19 @@ public class ArrayRataNilai15 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int lulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                lulus += 1;
+            }
         }
         rata2 = total / nilaiMhs.length;
         System.out.println("rata-rata nilai = " + rata2);
+        System.out.println("Banyaknya mahasiswa yang lulus adalah " + lulus + " orang");
     }
 }
