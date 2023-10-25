e344d0e2e9c519e084be9f026659a03ff2fae346
sukinnamz <innama.maesaputri@gmail.com>
2023-10-25 10:42:25 +0700

    Pertanyaan Percobaan 4 Searching

--- daspro-jobsheet9/LinearSearch15.java
+++ daspro-jobsheet9/LinearSearch15.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class LinearSearch15 {
     public static void main(String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int banyak = sc.nextInt();
+        int[] arrayInt = new int[banyak];
+        System.out.print("Masukkan key yang ingin dicari : ");
+        int key = sc.nextInt();
         int hasil = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+                break;
+            } else {
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+       
     }
 }
