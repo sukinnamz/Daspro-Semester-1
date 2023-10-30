6b35550f010f975d6fa9aed6d642d61714cabefc
sukinnamz <innama.maesaputri@gmail.com>
2023-10-30 13:34:50 +0700

    Percobaan 2 Pertanyaan 1

--- daspro-jobsheet10/BioskopWithScanner15.java
+++ daspro-jobsheet10/BioskopWithScanner15.java
@@ -4,24 +4,46 @@ public class BioskopWithScanner15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
-        int baris, kolom;
+        int baris, kolom, menu = 0, lagi;
         String nama, next;
         String[][] penonton = new String[4][2];
-        while (true) {
-            System.out.print("Masukkan nama : ");
-            nama = sc2.nextLine();
-            System.out.print("Masukkan baris : ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom : ");
-            kolom = sc.nextInt();
+        while (menu != 3) {
+            System.out.println("============================");
+            System.out.println("Pilih menu :");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Masukkan nomor menu : ");
+            menu = sc.nextInt();
+            switch (menu) {
+                case 1:
+                    while (true) {
+                        System.out.print("Masukkan nama : ");
+                        nama = sc2.nextLine();
+                        System.out.print("Masukkan baris : ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom : ");
+                        kolom = sc.nextInt();
 
-            penonton[baris - 1][kolom - 1] = nama;
+                        penonton[baris - 1][kolom - 1] = nama;
 
-            System.out.print("Input penonton lainnya? (y/n) : ");
-            next = sc2.nextLine();
+                        System.out.print("Input penonton lainnya? (y/n) : ");
+                        next = sc2.nextLine();
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Daftar penonton :");
+                    for (int i = 0; i < penonton.length; i++) {
+                        System.out
+                                .println("Penonton pada baris ke-" + (i + 1) + " : " + String.join(", ", penonton[i]));
+                    }
+                    break;
+                default:
+                    break;
             }
         }
     }
