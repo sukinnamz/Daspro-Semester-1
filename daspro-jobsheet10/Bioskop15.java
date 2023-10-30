c24415bd5a5bf9db0f95cf8fbd3748e203a86ad7
sukinnamz <innama.maesaputri@gmail.com>
2023-10-30 12:18:06 +0700

    Percobaan 1 Pertanyaan 10

--- daspro-jobsheet10/Bioskop15.java
+++ daspro-jobsheet10/Bioskop15.java
@@ -19,5 +19,10 @@ public class Bioskop15 {
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris : " + barisPenonton.length);
         }
+
+        System.out.println("Penonton pada baris ke-3 : ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }
