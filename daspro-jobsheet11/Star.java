76aea9b97c93ef2a7a773d1b5572372b7c495283
sukinnamz <innama.maesaputri@gmail.com>
2023-11-06 12:56:30 +0700

    Jobsheet 11 Percobaan 2

--- daspro-jobsheet11/Star.java
+++ daspro-jobsheet11/Star.java
@@ -5,8 +5,11 @@ public class Star {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
-            System.out.print("*");
+        for (int iOuter = 0; iOuter <= N; iOuter++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
