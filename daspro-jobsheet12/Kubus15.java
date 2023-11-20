f0bd5d5594c56941dddcddfb1f1391d906f67a39
sukinnamz <innama.maesaputri@gmail.com>
2023-11-20 14:46:50 +0700

    Tugas 1

---
+++ daspro-jobsheet12/Kubus15.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Kubus15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan panjang sisi kubus : ");
+        int sisi = sc.nextInt();
+        System.out.println("Volume : " + volume(sisi));
+        System.out.println("Luas Permukaan : " + luasper(sisi));
+    }
+
+    public static int volume(int sisi) {
+        int vol = sisi * sisi;
+        return vol;
+    }
+
+    public static int luasper(int sisi) {
+        int luas = 6 * sisi * sisi;
+        return luas;
+    }
+}
