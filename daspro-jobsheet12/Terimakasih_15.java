aa48c321e210fb61d5354ab4777f26b8b4f43bac
sukinnamz <innama.maesaputri@gmail.com>
2023-11-20 12:12:04 +0700

    Percobaan 3

--- daspro-jobsheet12/Terimakasih_15.java
+++ daspro-jobsheet12/Terimakasih_15.java
@@ -1,10 +1,16 @@
 public class Terimakasih_15 {
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terimakasih pak bu semoga sehat selalu";
+        UcapanTambahan(ucapan);
     }
 
     public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacjer in the world. \n" +
                 "You inspired in me a love for learning and made me feel like I could ask you anything");
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
     }
 }
