5c3d6cdf5d638315f41f8307e1cb9b9b5fbf31be
sukinnamz <innama.maesaputri@gmail.com>
2023-11-20 12:11:48 +0700

    Percobaan 2

---
+++ daspro-jobsheet12/Ucapan_15.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ucapan_15 {
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan nama orang yang ingin anda beri ucapan : ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+"\nMay the force be with you you.");
+    }
+}
